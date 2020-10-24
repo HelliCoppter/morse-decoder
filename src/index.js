5786a58f2c5111e78387773af713c7cc00e3b403
Alexander Fufachev <alexfufachev@mail.ru>
2020-10-24 12:39:22 +0300

    feat: done morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let stepArray = []
+    let arr = expr.split('')
+    let helpArray = []
+    let symbol = ''
+
+    for (let num of arr) {
+        helpArray.push(num) 
+        if (helpArray.length === 10) {
+            stepArray.push(helpArray.join(''))
+            helpArray.length = 0
+        }
+    }
+
+    for (let item of stepArray) {
+        let arr = item.split('')
+
+        for (let i=0; i < arr.length; i += 2) {
+            if (arr[i] === '1') {
+                if (arr[i+1] === '0') { symbol += '.'}
+                if (arr[i+1] === '1') { symbol += '-'}
+            }
+        }
+
+        if (item === '**********') {
+          helpArray.push(' ')
+        }
+        helpArray.push(symbol)
+        symbol = ''
+    }
+
+    stepArray.length = 0
+
+    for (let item of helpArray) {
+      if (item === ' ') {
+          stepArray.push(item)
+      } else {
+        stepArray.push(MORSE_TABLE[item])
+      }
+    }
+
+    return stepArray.join('')
 }
 
 module.exports = {
